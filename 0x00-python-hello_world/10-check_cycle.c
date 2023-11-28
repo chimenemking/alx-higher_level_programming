cf7a42ec6eb027236ad1688c9a769784ccb960fe
chimenemking <chimenemwillis@gmail.com>
2023-11-27 18:24:06 -0800

    detect cycle in a list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if list has a cycle
+ * @list: linked list
+ * Return: 1 shows cycle , 0 shows none.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (!list || !list->next)
+		return (0);
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}

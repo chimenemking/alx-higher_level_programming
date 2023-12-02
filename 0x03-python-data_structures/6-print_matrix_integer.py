a9f75e4dcfda1d59950c515839458e7a2bd62d85
chimenemking <chimenemwillis@gmail.com>
2023-12-01 18:19:01 -0800

    Data structures, lists anf tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for idx1 in range(len(matrix)):
+        for idx2 in range(len(matrix[idx1])):
+            if idx2 != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[idx1][idx2]), end="")
+        print()

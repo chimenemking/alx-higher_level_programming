5673886bea8dff218ee7cc719e33312a65494c7e
chimenemking <87863371+chimenemking@users.noreply.github.com>
2023-12-05 02:47:38 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-        x1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+        if len(tuple_a) >= 1:
+        x1 = tuple_a[0]
+    else:
+        x1 = 0
     
     if len(tuple_a) >= 2:
         x2 = tuple_a[1]

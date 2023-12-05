767f6b565bbba2ec1aa11ce1866d585a7fc2ed22
chimenemking <87863371+chimenemking@users.noreply.github.com>
2023-12-05 03:09:20 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    x1, x2 = tuple_a[:2]
-    y1, y2 = tuple_b[:2]
-    return x1 + y1, x2 + y2
+    if len(tuple_a) >= 1:
+        x1 = tuple_a[0]
+    else:
+        x1 = 0
+    if len(tuple_a) >= 2:
+        x2 = tuple_a[1]
+    else:
+        x2 = 0
+
+    if len(tuple_b) >= 1:
+        y1 = tuple_b[0]
+    else:
+        y1 = 0
+    if len(tuple_b) >= 2:
+        y2 = tuple_b[1]
+    else:
+        y2 = 0
+
+    return a1 + b1, a2 + b2

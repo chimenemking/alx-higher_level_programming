9507196403307ba1d31740f11aeedb9e35546e22
chimenemking <chimenemwillis@gmail.com>
2023-12-06 17:53:11 -0800

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for search_idx in range(len(my_list)):
+        if my_list[search_idx] == search:
+            new_list.append(replace)
+            continue
+        new_list.append(my_list[search_idx])
+
+    return new_list

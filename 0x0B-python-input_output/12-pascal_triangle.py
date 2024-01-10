b5dbbb6910735992ba7bbdaafd6e2c16abefde94
Chimenem Okandu <87863371+chimenemking@users.noreply.github.com>
2024-01-10 02:20:07 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""pascal_traigle module."""
+"""pascal's triangle"""
 
 
 def pascal_triangle(n):
-    """ pascal traigle class body.
-    """
+
     if n <= 0:
         return []
-
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return
+    temp = []
+    cpy = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if i == 0 or j == 0 or i == j:
+                row.append(1)
+            else:
+                row.append(cpy[j] + cpy[j - 1])
+        cpy = row
+        temp.append(row)
+    return temp
